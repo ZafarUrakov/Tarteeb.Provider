24ba9052da7de3bee1c448887945ed0db54fe998
Zafar <icom@MACKBOOK>
2023-10-07 00:31:02 +0300

    FOUNDATIONS: Import Applicant

--- Tarteeb.Provider/Program.cs
+++ Tarteeb.Provider/Program.cs
@@ -3,10 +3,35 @@
 // Powering True Leadership
 //===============================
 
-internal class Program
+using System;
+using System.Collections.Generic;
+using Tarteeb.Provider.Brokers.Spreadsheets;
+using Tarteeb.Provider.Models.Applicant;
+using Tarteeb.Provider.Services.Foundatons.ImporterService;
+
+class Program
 {
-    private static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello");
+        string filePath = @"C:\Users\icom\Desktop\.net.xlsx"; // Замените на путь к вашему файлу
+
+        // Создаем объект SpreadsheetBroker
+        SpreadsheetBroker spreadsheetBroker = new SpreadsheetBroker();
+
+        // Импортируем список заявителей
+        List<Applicant> importedApplicants = spreadsheetBroker.ImportApplicantToList(filePath);
+
+        // Создаем объект ApplicantProcessor
+
+        ImporterService importerService = new ImporterService();
+        // Фильтруем и сортируем заявителей
+        List<Applicant> processedApplicants = importerService.ValidateApplicantNotNull(importedApplicants);
+
+        // Выводим данные заявителей на консоль
+        foreach (var applicant in processedApplicants)
+        {
+            Console.WriteLine($"Firstname: {applicant.Firstname} , LastName: {applicant.Lastname}, " +
+                $"PhoneNumber: {applicant.PhoneNumber}, Email: {applicant.Email} Groupname: {applicant.Groupname}");
+        }
     }
 }
---
+++ Tarteeb.Provider/Services/Foundations/ImporterService/ImporterService.cs
@@ -0,0 +1,33 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//===============================
+
+using System.Collections.Generic;
+using Tarteeb.Provider.Brokers.Spreadsheets;
+using Tarteeb.Provider.Models.Applicant;
+
+namespace Tarteeb.Provider.Services.Foundatons.ImporterService
+{
+    internal class ImporterService
+    {
+        internal List<Applicant> ValidateApplicantNotNull(List<Applicant> applicants)
+        {
+            List<Applicant> filteredApplicants = new List<Applicant>();
+
+            foreach (var applicant in applicants)
+            {
+                if (applicant != null)
+                {
+                    filteredApplicants.Add(applicant);
+                }
+                else
+                {
+                
+                }
+            }
+
+            return filteredApplicants;
+        }
+    }
+}
