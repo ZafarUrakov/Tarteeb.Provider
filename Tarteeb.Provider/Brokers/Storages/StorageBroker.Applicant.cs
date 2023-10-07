e49dd05868efafe43164424701c84d1f0d43ef7e
Zafar <icom@MACKBOOK>
2023-10-07 10:00:36 +0300

    BROKERS: Create All Methods

--- Tarteeb.Provider/Brokers/Storages/StorageBroker.Applicant.cs
+++ Tarteeb.Provider/Brokers/Storages/StorageBroker.Applicant.cs
@@ -4,6 +4,9 @@
 //===============================
 
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Tarteeb.Provider.Models.Applicant;
 
 namespace Tarteeb.Provider.Brokers.Storages
@@ -11,5 +14,40 @@ namespace Tarteeb.Provider.Brokers.Storages
     internal partial class StorageBroker
     {
         public DbSet<Applicant> Applicants { get; set; }
+
+        public async Task<Applicant> InsertApplicantAsync(Applicant applicant)
+        {
+            await this.Applicants.AddAsync(applicant);
+            await SaveChangesAsync();
+
+            return applicant;
+        }
+
+        public async Task<Applicant> SelectApplicantById(Guid applicantId) =>
+            await this.Applicants.FindAsync(applicantId);
+
+        public IQueryable<Applicant> SelectAll() =>
+            this.Applicants.AsQueryable();
+
+        public async Task<Applicant> UpdateApplicantAsync(Applicant applicant)
+        {
+            this.Applicants.Update(applicant);
+            await SaveChangesAsync();
+
+            return applicant;
+        }
+
+        public async Task<bool> DeleteApplicantAsync(Guid applicantId)
+        {
+            var applicantToDelete = await this.Applicants.FindAsync(applicantId);
+
+            if (applicantToDelete == null)
+                return false;
+
+            this.Applicants.Remove(applicantToDelete);
+            await this.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
