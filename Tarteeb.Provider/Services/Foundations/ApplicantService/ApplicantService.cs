7c619e7d238dd9e9c387c7620ac226affc0352ee
Zafar <icom@MACKBOOK>
2023-10-07 10:12:29 +0300

    FOUNDATIONS: Add Applicant

---
+++ Tarteeb.Provider/Services/Foundations/ApplicantService/ApplicantService.cs
@@ -0,0 +1,24 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//===============================
+
+using System.Threading.Tasks;
+using Tarteeb.Provider.Brokers.Storages;
+using Tarteeb.Provider.Models.Applicant;
+
+namespace Tarteeb.Provider.Services.Foundations.ApplicantService
+{
+    internal class ApplicantService
+    {
+        private readonly StorageBroker storageBroker;
+
+        public ApplicantService(StorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        internal Task<Applicant> AddApplicantAsync(Applicant applicant) =>
+            this.storageBroker.InsertApplicantAsync(applicant);
+    }
+}
